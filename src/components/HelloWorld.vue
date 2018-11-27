b22058fa7c5acc05a5635870e52aef0e5361bebd
Roc Wang <roc@kiwiberry.nz>
2018-11-28 09:16:46 +1300

    play with the API

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,109 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+  <div>
+    <div style="float:right">
+      <h4>Stop Time</h4>
+      <ul>
+        <li v-for="stopTime in stopTimes" :key="stopTime.stop_sequence">
+          <button>{{stopTime.arrival_time}} - {{stopTime.departure_time}}</button>
+        </li>
+      </ul>
+      <h4>realtimeTransitFeed</h4>
+      <p>{{realtimeTransitFeed}}</p>
+    </div>
+    <h4>Stops</h4>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-e2e-cypress" target="_blank" rel="noopener">e2e-cypress</a></li>
+      <li v-for="stop in stops" :key="stop.stop_id">
+        <button @click="selectedStopId = stop.stop_id">{{stop.stop_name}}</button>
+      </li>
     </ul>
-    <h3>Essential Links</h3>
+    <h4>Routes</h4>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+      <li v-for="route in routes" :key="route.route_id">
+        <button @click="selectedRouteId = route.route_id">{{route.route_short_name}}- {{route.route_long_name}}</button>
+      </li>
     </ul>
-    <h3>Ecosystem</h3>
+    <h4>Trips</h4>
     <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li v-for="trip in trips" :key="trip.trip_id">
+        <button @click="selectedTripId = trip.trip_id">{{trip.trip_headsign}}</button>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+const subscriptionKey = "a67afd3d4e9e4494a2cae4b310556b35";
+const myLocation = {
+  latitude: -36.7961008,
+  longitude: 174.647264
+};
+
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String
+  data() {
+    return {
+      stops: [],
+      selectedStopId: "",
+      routes: [],
+      selectedRouteId: "",
+      trips: [],
+      selectedTripId: "",
+      stopTimes: [],
+      realtimeTransitFeed: []
+    };
+  },
+  async created() {
+    this.stops = await this.getStops(myLocation);
+  },
+  watch: {
+    async selectedStopId() {
+      this.routes = await this.getRoutesByStop(this.selectedStopId);
+    },
+    async selectedRouteId() {
+      this.trips = await this.getTripsByRoute(this.selectedRouteId);
+    },
+    async selectedTripId() {
+      this.stopTimes = await this.getStopTimesByTrip(this.selectedTripId);
+      this.realtimeTransitFeed = await this.getRealtimeTransitFeed(
+        this.selectedTripId
+      );
+    }
+  },
+  methods: {
+    callApi(request) {
+      return fetch(request, {
+        headers: { "Ocp-Apim-Subscription-Key": subscriptionKey }
+      })
+        .then(response => response.json())
+        .then(json => json.response);
+    },
+    getStops(location) {
+      return this.callApi(
+        `https://api.at.govt.nz/v2/gtfs/stops/geosearch?lat=${
+          location.latitude
+        }&lng=${location.longitude}&distance=300`
+      );
+    },
+    getRoutesByStop(stopId) {
+      return this.callApi(
+        `https://api.at.govt.nz/v2/gtfs/routes/stopid/${stopId}`
+      );
+    },
+    getTripsByRoute(routeId) {
+      return this.callApi(
+        `https://api.at.govt.nz/v2/gtfs/trips/routeid/${routeId}`
+      );
+    },
+    getStopTimesByTrip(tripId) {
+      return this.callApi(
+        `https://api.at.govt.nz/v2/gtfs/stopTimes/tripId/${tripId}`
+      );
+    },
+    getRealtimeTransitFeed(tripId) {
+      return this.callApi(
+        `https://api.at.govt.nz/v2/public/realtime/?tripid=${tripId}`
+      );
+    }
   }
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
