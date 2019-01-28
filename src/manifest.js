be0f076b49b469629d16fc901fb3dc7b24af413a
Roc Wang <roc@kiwiberry.nz>
2019-01-28 13:23:44 +1300

    use webapp-webpack-plugin for web app manifest and favicons

--- src/manifest.js
+++ src/manifest.js
@@ -1,20 +1,15 @@
-const path = require("path");
+const packageJson = require("../package");
 
 module.exports = {
   name: "Bus",
   short_name: "Bus",
-  icons: [
-    {
-      src: path.join(__dirname, "assets/icon.png"),
-      sizes: [192, 512],
-      destination: "img"
-    }
-  ],
+  description: packageJson.description,
   lang: "en",
   orientation: "portrait-primary",
   scope: "/",
   start_url: "/",
   display: "standalone",
+  dir: "ltr",
   background_color: "#FFDB1B",
   theme_color: "#001120"
 };
--- vue.config.js
+++ vue.config.js
@@ -1,31 +1,39 @@
 const os = require("os");
 const fs = require("fs");
-const WebpackPwaManifest = require("webpack-pwa-manifest");
-const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 const manifest = require("./src/manifest");
 const workboxWebpackPlugin = require("workbox-webpack-plugin");
 const packageJson = require("./package");
+const WebappWebpackPlugin = require("webapp-webpack-plugin");
 
 module.exports = {
   configureWebpack: {
     plugins: [
-      // generate web app manifest
-      new WebpackPwaManifest({
-        ...manifest,
-        filename: "manifest.[hash:8].webmanifest"
-      }),
-      // generate favicons
-      new FaviconsWebpackPlugin({
+      new WebappWebpackPlugin({
         logo: "./src/assets/icon.png",
-        prefix: "img/[hash:8]-",
-        persistentCache: false,
-        icons: {
-          android: false,
-          appleIcon: false,
-          appleStartup: false,
-          favicons: true,
-          firefox: false
-        }
+        prefix: "favicons-[hash:8]",
+        inject: htmlPlugin => {
+          console.log(htmlPlugin);
+          return true;
+        },
+        favicons: Object.assign(
+          {
+            appName: manifest.name,
+            appShortName: manifest.short_name,
+            appDescription: manifest.description,
+            background: manifest.background_color,
+            icons: {
+              android: true,
+              appleIcon: false,
+              appleStartup: false,
+              coast: false,
+              favicons: true,
+              firefox: false,
+              windows: false,
+              yandex: false
+            }
+          },
+          manifest
+        )
       })
     ]
   },
@@ -43,7 +51,7 @@ module.exports = {
     ) {
       config.plugin("workbox").use(workboxWebpackPlugin.GenerateSW, [
         {
-          exclude: [/\.map$/, /^iconstats/],
+          exclude: [/\.map$/, /manifest\.json$/, /^iconstats/],
           cacheId: packageJson.name
         }
       ]);
