746bdd4f17e69e3ccd87a97804cf67570b67a1f8
Roc Wang <roc@kiwiberry.nz>
2019-06-09 14:27:47 +1200

    refactor router and workBox

--- src/components/UpdateNotification.vue
+++ src/components/UpdateNotification.vue
@@ -21,7 +21,7 @@
 <script>
 import Buttonizer from "../components/Buttonizer";
 import Notification from "./Notification";
-import { workBox } from "../registerServiceWorker";
+import { workBox } from "../workBox";
 
 export default {
   name: "UpdateNotification",
@@ -32,38 +32,34 @@ export default {
     };
   },
   created() {
-    if (workBox) {
-      // Add an event listener to detect when the registered
-      // service worker has installed but is waiting to activate.
-      workBox.addEventListener("waiting", () => {
-        // `event.wasWaitingBeforeRegister` will be false if this is
-        // the first time the updated service worker is waiting.
-        // When `event.wasWaitingBeforeRegister` is true, a previously
-        // updated same service worker is still waiting.
-        // You may want to customize the UI prompt accordingly.
-        this.isVisible = true;
-      });
-    }
+    // Add an event listener to detect when the registered
+    // service worker has installed but is waiting to activate.
+    workBox.addEventListener("waiting", () => {
+      // `event.wasWaitingBeforeRegister` will be false if this is
+      // the first time the updated service worker is waiting.
+      // When `event.wasWaitingBeforeRegister` is true, a previously
+      // updated same service worker is still waiting.
+      // You may want to customize the UI prompt accordingly.
+      this.isVisible = true;
+    });
   },
   methods: {
     close() {
       this.isVisible = false;
     },
     update() {
-      if (workBox) {
-        // Assuming the user accepted the update, set up a listener
-        // that will reload the page as soon as the previously waiting
-        // service worker has taken control.
-        workBox.addEventListener("controlling", () => {
-          window.location.reload();
-        });
+      // Assuming the user accepted the update, set up a listener
+      // that will reload the page as soon as the previously waiting
+      // service worker has taken control.
+      workBox.addEventListener("controlling", () => {
+        window.location.reload();
+      });
 
-        // Send a message telling the service worker to skip waiting.
-        // This will trigger the `controlling` event handler above.
-        // Note: for this to work, you have to add a message
-        // listener in your service worker. See below.
-        workBox.messageSW({ type: "SKIP_WAITING" });
-      }
+      // Send a message telling the service worker to skip waiting.
+      // This will trigger the `controlling` event handler above.
+      // Note: for this to work, you have to add a message
+      // listener in your service worker. See below.
+      workBox.messageSW({ type: "SKIP_WAITING" });
     }
   }
 };
--- src/main.js
+++ src/main.js
@@ -3,15 +3,52 @@ import "./assets/global.module.css";
 
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
+import Router from "vue-router";
+import Intro from "./views/Intro";
+import Favourites from "./views/Favourites";
+import Stop from "./views/Stop";
+import Route from "./views/Route";
 import VueRx from "vue-rx";
 import "pwacompat";
-import "./registerServiceWorker";
+import "./workBox";
 
 Vue.config.productionTip = false;
+Vue.use(Router);
 Vue.use(VueRx);
 
 new Vue({
-  router,
+  router: new Router({
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes: [
+      {
+        path: "/",
+        name: "Intro",
+        component: Intro
+      },
+      {
+        path: "/favourites",
+        name: "Favourites",
+        component: Favourites,
+        props: route => ({
+          isCollapsedInitially: route.query.isCollapsed === "yes"
+        })
+      },
+      {
+        path: "/stop/:stopCode",
+        name: "Stop",
+        component: Stop
+      },
+      {
+        path: "/stop/:stopCode/route_group/:shortName",
+        name: "Route",
+        component: Route
+      },
+      {
+        path: "*",
+        redirect: { name: "Intro" }
+      }
+    ]
+  }),
   render: createElement => createElement(App)
 }).$mount("#app");
