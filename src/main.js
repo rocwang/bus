529bde7867e2144a2d5c6293ffafb45df2059e77
Roc Wang <roc@kiwiberry.nz>
2019-11-06 07:10:07 +1300

    introduce vuex

--- src/main.js
+++ src/main.js
@@ -7,12 +7,14 @@ import router from "./router";
 import VueRx from "vue-rx";
 import "pwacompat";
 import { workBox } from "./workBox";
+import store from "./store";
 
 Vue.config.productionTip = false;
 Vue.use(VueRx);
 
 new Vue({
   router,
+  store,
   render: createElement => createElement(App)
 }).$mount("#app");
 
